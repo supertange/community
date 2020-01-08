e7e8fa258a36bee7d362bfca86fd49424303e40c
supertange <supertange@outlook.com>
2020-01-08 12:34:16 +0800

    add index bootstrap

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ https://spring.io
 http://elasticsearch.cn/explore
 
 ## 工具
+> github oath app build link:
+>
+> https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
--- src/main/java/com/supertange/community/community/controller/IndexController.java
+++ src/main/java/com/supertange/community/community/controller/IndexController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class IndexController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name",name);
-        return "greet";
+        return "index";
     }
 
 }
