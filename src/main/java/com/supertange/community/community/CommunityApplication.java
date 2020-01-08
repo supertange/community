5b7e2f69b75bce7328f91255aca0254f3c503d79
Si方 <supertange@outlook.com>
2020-01-08 10:50:45 +0800

    init

---
+++ src/main/java/com/supertange/community/community/CommunityApplication.java
@@ -0,0 +1,13 @@
+package com.supertange.community.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/supertange/community/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supertange.community.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
