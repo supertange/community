dcedbfde3c5f1d7514e28baa27aa46ea34542273
supertange <supertange@outlook.com>
2020-01-08 21:31:03 +0800

    add h2 mybatis

--- README.md
+++ README.md
@@ -14,3 +14,19 @@
 ## 工具
 [Visual Paradigm](https://www.visual-paradigm.com)
 
+## 脚本
+```sql
+create table USER
+(
+    ID INT  auto_increment primary key not null,
+    ACCOUNT_ID VARCHAR(100),
+    NAME VARCHAR(50),
+    TOKEN CHAR(36),
+    GMT_CREATE BIGINT,
+    GMT_MODIFIED BIGINT
+);
+
+comment on table USER is '用户';
+
+
+```
--- src/main/java/com/supertange/community/community/controller/AuthorizeController.java
+++ src/main/java/com/supertange/community/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.supertange.community.community.controller;
 
 import com.supertange.community.community.dto.AccessTokenDTO;
 import com.supertange.community.community.dto.GithubUser;
+import com.supertange.community.community.mapper.UserMapper;
+import com.supertange.community.community.model.User;
 import com.supertange.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,15 +11,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+    @Autowired
+    private UserMapper userMapper;
 
-    @Value("${github.client_id}")
+    @Value("${github.client.id}")
     private String clientId;
-    @Value("${github.client_secret}")
+    @Value("${github.client.secret}")
     private String clientSecret;
     @Value("${github.redirect.url}")
     private String redirectUrl;
@@ -26,7 +33,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUrl);
@@ -35,7 +43,20 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken=githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
-        System.out.println(githubUser.toString());
-        return "index";
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(System.currentTimeMillis());
+            userMapper.insert(user);
+            //登录成功 写cookie session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else {
+            //登录失败
+            return "/";
+        }
     }
 }
