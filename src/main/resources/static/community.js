deea907ee457a57c131a239d8bf4ab71db7f8592
supertange <supertange@outlook.com>
2020-01-14 14:23:19 +0800

    fix jump to another page

--- src/main/resources/static/community.js
+++ src/main/resources/static/community.js
@@ -4,17 +4,25 @@ function postComment() {
     $.ajax({
         type: "POST",
         url: "/comment",
-        contentType:"application/json",
+        contentType: "application/json",
         data: JSON.stringify({
             "parentId": questionId,
             "content": content,
             "type": 1
         }),
         success: function (response) {
-            if (response.code==200){
+            if (response.code == 200) {
                 $("#comment_section").hide()
-            }else {
-                console.log(response.message)
+            } else {
+                if (response.code=2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=Iv1.81af48c6c8c2952b&redirect_uri=http://localhost:8080/callback&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message)
+                }
             }
         },
         dataType: "json"
