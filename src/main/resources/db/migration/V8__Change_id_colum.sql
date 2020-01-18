db613569c0fd60a173f9b51685ab12ffc5ea8d0f
supertange <supertange@outlook.com>
2020-01-18 22:20:13 +0800

    修复数据库脚本

--- src/main/resources/db/migration/V8__Change_id_colum.sql
+++ src/main/resources/db/migration/V8__Change_id_colum.sql
@@ -1,3 +1,4 @@
-alter table comment alter column commentator bigint;
-alter table question alter column creator bigint;
+
+alter table comment modify commentator bigint ;
+alter table `user` modify creator bigint;
 
